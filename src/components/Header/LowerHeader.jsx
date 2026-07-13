f3a5f5aa9f297e372511ce7fb86fe5d5abdb857e
rhyshaven <tehhmuhh@gmail.com>
2026-07-13 14:23:58 -0500

    fix: linked Sell button to the secure add-product route

--- src/components/Header/LowerHeader.jsx
+++ src/components/Header/LowerHeader.jsx
@@ -1,28 +1,26 @@
 import React from "react";
-import { MdOutlineMenu } from "react-icons/md"; // Importing a menu icon from react-icons
-import classes from "./Header.module.css"; // Importing CSS module for styling specific to the LowerHeader component
+import { Link } from "react-router-dom"; // 1. Import Link
+import classes from "./Header.module.css"; // Ensure this matches your CSS file
 
-// Function component that renders the lower part of the header
 function LowerHeader() {
   return (
     <div className={classes.lower__container}>
-      {/* Container for lower header items */}
       <ul>
-        {/* List of navigation options */}
+        <li>All</li>
+        <li>Today's Deals</li>
+        <li>Customer Services</li>
+        <li>Registry</li>
+        <li>Gift Cards</li>
+        
+        {/* 2. Wrap "Sell" in a Link pointing to /add-product */}
         <li>
-          {/* Menu icon and "All" option */}
-          <MdOutlineMenu /> {/* Menu icon */}
-          <p>All</p> {/* "All" label next to the icon */}
+          <Link to="/add-product" style={{ color: "white", textDecoration: "none" }}>
+            Sell
+          </Link>
         </li>
-        {/* Other navigation links */}
-        <li>Today's Deals</li> {/* Static link for "Today's Deals" */}
-        <li>Customer Services</li> {/* Static link for "Customer Services" */}
-        <li>Registry</li> {/* Static link for "Registry" */}
-        <li>Gift Cards</li> {/* Static link for "Gift Cards" */}
-        <li>Sell</li> {/* Static link for "Sell" */}
       </ul>
     </div>
   );
 }
 
-export default LowerHeader; // Export the component for use in other parts of the application
+export default LowerHeader;
